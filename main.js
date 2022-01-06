5c08cc6c06f291471da76746f3878cb70d2215de
KamimuraY <y.kamimura@i3design.co.jp>
2022-01-06 14:23:36 +0900

    Fix: gsap practice

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-gsap.to("h1", {x: 100})
+gsap.to('.title .text', {
+  y: 0,
+  duration: 0.1,
+});
+
+gsap.from('.html-logo', {
+  ease: 'bounce.out',
+  y: -500,
+  duration: 2,
+});
+gsap.from('.shadow', {
+  ease: 'bounce.out',
+  duration: 2,
+  css: {
+    scaleX: 0,
+    scaleY: 0,
+  },
+});
+
+document.addEventListener('mousemove', (event) => {
+  gsap.to('.cursor', {
+    x: event.pageX - 20,
+    y: event.pageY - 20,
+  });
+});
